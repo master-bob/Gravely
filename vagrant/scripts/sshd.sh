e41f9ce02c717810254b4e439fa121d942667279
Leonardo Canessa <masterbob@gmail.com>
2016-02-07 18:18:21 -0500

    WIP More SSHD imrpovements

--- vagrant/scripts/sshd.sh
+++ vagrant/scripts/sshd.sh
@@ -7,20 +7,37 @@ install_sshd() {
 }
 
 config_sshd() {
-    sed -ri 's/#Port 22/Port $port/g' /etc/ssh/sshd_config
-    sed -ri 's/#LogLevel INFO/LogLevel VERBOSE/g' /etc/ssh/sshd_config
-    sed -ri 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
+    #Copy default config
+    cp /sshd_config /etc/ssh/sshd_config
 
-    # Taken from Mozilla's infoSec team: Securing OpenSSH
-    printf 'HostKey /etc/ssh/ssh_host_ed25519_key
-HostKey /etc/ssh/ssh_host_rsa_key
-HostKey /etc/ssh/ssh_host_ecdsa_key
-     
-KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256
-     
-Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
-     
-MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com' >> /etc/ssh/sshd_config
+    # Insert custom parameters
+    sed -ri 's/#Port 22/Port `$port`/g' /etc/ssh/sshd_config
+
+    #rm default config
+    rm /sshd_config
+
+    #Generate custom server keys
+    cd /etc/ssh
+    rm ssh_host_*key*
+    ssh-keygen -t ed25519 -f ssh_host_ed25519_key < /dev/null
+    ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key < /dev/null
+
+    #generate client keys
+    #ssh-keygen -t ed25519 -o -a 100 -f ~/.ssh/id_ed25519
+    #ssh-keygen -t rsa -b 4096 -o -a 100 -f ~/.ssh/id_rs
+
+    # Create ssh-user group
+    sudo groupadd ssh-user
+
+    # add user to group
+    sudo usermod -a -G ssh-user $user
+
+    # Copy user key to correct location
+    mkdir ~/.ssh
+    chmod 700 ~/.ssh
+    cat /db_id_ecdsa.pub >> ~/.ssh/authorized_keys
+    rm /db_id_ecdsa.pub
+    chmod 600 ~/.ssh/authorized_keys
 }
 
 install_sshd
@@ -29,7 +46,5 @@ while [ $? -ne 0]; do install_sshd; done
 config_sshd
 while [ $? -ne 0]; do config_sshd; done
 
-
-chmod 400 ~/.ssh/authorized_keys
 #TODO: this should be changed to sshd.socket
 systemctl start sshd.service
